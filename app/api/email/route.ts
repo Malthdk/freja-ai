733d9feb533ddd9d4a7949bd809cae8e01f7f0d1
Malthdk <malte.holledig@zeronorth.com>
2024-08-04 12:28:11 +0200

    fix: email

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -7,14 +7,15 @@ export async function POST(request: NextRequest) {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   try {
-    return resend.emails.send({
+    resend.emails.send({
       from: "onboarding@resend.dev",
       to: "malte@holledig.dk",
       subject: `Freja.ai lead: ${name}`,
       html: `<h3>Name: ${name}</h3><h3>Email: ${email}</h3><h3>Workplace: ${workplace}</h3><h3>Phone: ${phone}</h3><h3>Message: ${message}</h3>`,
-    }) as unknown as Response;
+    });
+    return new Response("Email sent", { status: 200 });
   } catch (error) {
     console.error(error);
-    return new Response("Failed to send email" + error, { status: 500 });
+    return new Response("Failed to send email", { status: 500 });
   }
 }
